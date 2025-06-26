6fce80dbfa36c76d48aeb456834feedac74ae1a7
arun-19n <nuraruran32@gmail.com>
2025-06-26 10:07:49 +0530

    2ndl commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Arun-s__Profile/', // Match your repo name EXACTLY
 })
